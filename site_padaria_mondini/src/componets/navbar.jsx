95e5298275443c97eeab66320c75e539a05a99b4
karineee3 <karineval3445@gmail.com>
2024-07-11 12:18:36 -0300

    atualização de lista de links
    criação de links de midias sociais

--- site_padaria_mondini/src/componets/navbar.jsx
+++ site_padaria_mondini/src/componets/navbar.jsx
@@ -1,3 +1,6 @@
+const numero = 48996803140
+const mensagem ="Olá,%20Bom%20dia.%0AGostaria%20de%20fazer%20um%20pedido"
+const link ='https:/wa.me/55'+numero+'?text='+mensagem
 
 
 
@@ -13,6 +16,13 @@ return(
           <a href="#produtos">Quem somos</a>
           <a href="#produtos">Trabalhe conosco</a>
         </div>
+        <div className="media">
+          <a  href={link} ><div className="medialink" id="wa" /></a>
+          <a  href="www.instagram.com"><div className="medialink" id="instagram" /></a>
+          
+
+        </div>
       </nav>
+
 )
 }export default Navbar
