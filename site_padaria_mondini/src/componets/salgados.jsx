9bfaddbad149fd54b062b0ccea827088e31d7ef9
karineee3 <karineval3445@gmail.com>
2024-08-05 16:42:57 -0300

    tentei fazer
    tentei fazer

---
+++ site_padaria_mondini/src/componets/salgados.jsx
@@ -0,0 +1,18 @@
+
+import '../layouts/produtos.css'
+function Salgados(){
+    return(
+        <div>
+            <h1>SALGADOS</h1>
+            <div className="produtos">
+                <div className="produto">
+                    <figure id={`salgados${2}`}></figure>
+                    <span>Salgados bons e deliciosos</span>
+
+
+                </div>
+
+            </div>
+        </div>
+    )
+}export default Salgados
